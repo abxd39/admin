e2ea309eda040e6068615b0df374f410f7e69523
xutonghua <287835375@qq.com>
2018-07-02 16:03:03 +0800

    session中间件

--- app/routes.go
+++ app/routes.go
@@ -3,9 +3,14 @@ package app
 import (
 	"admin/app/controller"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/sessions"
+	"admin/utils"
 )
 
 func Router(r *gin.Engine) {
+	//session
+	r.Use(sessions.Sessions("mysession", utils.Store))
+
 	new(controller.PublicController).Router(r)
 	new(controller.TestController).Router(r)
 	new(controller.WebsocketController).Router(r)
---
+++ utils/session.go
@@ -0,0 +1,42 @@
+package utils
+import (
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/redis"
+)
+
+var Store sessions.Store
+func init(){
+	stype,_:=Cfg.GetValue("session","type")
+
+	switch stype {
+	case "redis":
+		addr,err :=Cfg.GetValue("session","addr")
+		if err !=nil {
+			panic("utils.session:"+err.Error())
+		}
+		Store, err = redis.NewStore(10, "tcp", addr, "", []byte("secret"))
+		if err !=nil {
+			panic("utils.session:"+err.Error())
+		}
+	default:
+		panic("unkown session.type")
+	}
+
+}
+/* 使用示例
+r.GET("/incr", func(c *gin.Context) {
+	session := sessions.Default(c)
+	var count int
+	v := session.Get("count")
+	if v == nil {
+		count = 0
+	} else {
+		count = v.(int)
+		count++
+	}
+	session.Set("count", count)
+	session.Save()
+	c.JSON(200, gin.H{"count": count})
+})
+
+*/
