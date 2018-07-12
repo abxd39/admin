9e4d004fe94dd8a67243ce009b46c819ed16734b
陈伟伟 <326087571@qq.com>
2018-07-12 11:24:04 +0800

    优化封装的api返回结构

--- app/controller/baseController.go
+++ app/controller/baseController.go
@@ -4,13 +4,16 @@ import (
 	"admin/constant"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
 // 返回的结构
 type Response struct {
-	Code int                    `json:"code"`
-	Msg  string                 `json:"msg"`
-	Data map[string]interface{} `json:"data"`
+	Code int         `json:"code"`
+	Msg  string      `json:"msg"`
+	Data interface{} `json:"data"`
+
+	Result map[string]interface{} `json:"-"`
 }
 
 // base controller
@@ -20,17 +23,24 @@ type BaseController struct {
 
 // 设置返回的数据，key-value
 func (b *BaseController) Put(key string, value interface{}) {
-	if b.resp.Data == nil {
-		b.resp.Data = make(map[string]interface{})
+	if b.resp.Result == nil {
+		b.resp.Result = make(map[string]interface{})
 	}
 
-	b.resp.Data[key] = value
+	b.resp.Result[key] = value
 }
 
 // 正确的响应
 func (b *BaseController) RespOK(c *gin.Context, msg string) {
 	b.resp.Code = constant.RESPONSE_CODE_OK
 	b.resp.Msg = msg
+
+	if b.resp.Result != nil {
+		b.resp.Data = b.resp.Result
+	} else {
+		b.resp.Data = []int{} // 没有数据时，让data字段的json值为[]而非null
+	}
+
 	c.JSON(http.StatusOK, b.resp)
 }
 
@@ -39,5 +49,22 @@ func (b *BaseController) RespErr(c *gin.Context, code int, msg string) {
 	b.resp.Code = code
 	b.resp.Msg = msg
 
+	if b.resp.Result != nil {
+		b.resp.Data = b.resp.Result
+	} else {
+		b.resp.Data = []int{} // 没有数据时，让data字段的json值为[]而非null
+	}
+
 	c.JSON(http.StatusOK, b.resp)
+}
+
+// 获取get提交的int类型的参数，如果类型不是int，err不为nil
+// def表示默认值
+func (b *BaseController) GetInt(c *gin.Context, key string, def ...int) (int, error) {
+	param := c.Query(key)
+	if len(param) == 0 && len(def) == 1 {
+		return def[0], nil
+	}
+
+	return strconv.Atoi(param)
 }
