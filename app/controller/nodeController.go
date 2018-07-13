9b5ecf6e548c2d346b7801fe7acddca826df5491
陈伟伟 <326087571@qq.com>
2018-07-13 15:52:10 +0800

    ctx变量命名优化

--- app/controller/nodeController.go
+++ app/controller/nodeController.go
@@ -18,31 +18,31 @@ func (n *NodeController) Router(e *gin.Engine) {
 }
 
 // 节点列表
-func (n *NodeController) List(c *gin.Context) {
+func (n *NodeController) List(ctx *gin.Context) {
 	// 获取参数
-	page, err := n.GetInt(c, "page", 1)
+	page, err := n.GetInt(ctx, "page", 1)
 	if err != nil {
-		n.RespErr(c, "参数page格式错误")
+		n.RespErr(ctx, "参数page格式错误")
 		return
 	}
 
-	rows, err := n.GetInt(c, "rows", 10)
+	rows, err := n.GetInt(ctx, "rows", 10)
 	if err != nil {
-		n.RespErr(c, "参数rows格式错误")
+		n.RespErr(ctx, "参数rows格式错误")
 		return
 	}
 
 	// 调用model
 	list, err := new(backstage.Node).ListAll(page, rows, nil)
 	if err != nil {
-		n.RespErr(c, err)
+		n.RespErr(ctx, err)
 		return
 	}
 
 	// 设置返回数据
-	n.Put(c, "list", list)
+	n.Put(ctx, "list", list)
 
 	// 返回
-	n.RespOK(c)
+	n.RespOK(ctx)
 	return
 }
--- app/controller/roleController.go
+++ app/controller/roleController.go
@@ -24,41 +24,41 @@ func (r *RoleController) Router(e *gin.Engine) {
 }
 
 // 用户组列表
-func (r *RoleController) List(c *gin.Context) {
+func (r *RoleController) List(ctx *gin.Context) {
 	// 获取参数
-	page, err := r.GetInt(c, "page", 1)
+	page, err := r.GetInt(ctx, "page", 1)
 	if err != nil {
-		r.RespErr(c, "参数page格式错误")
+		r.RespErr(ctx, "参数page格式错误")
 		return
 	}
 
-	rows, err := r.GetInt(c, "rows", 10)
+	rows, err := r.GetInt(ctx, "rows", 10)
 	if err != nil {
-		r.RespErr(c, "参数rows格式错误")
+		r.RespErr(ctx, "参数rows格式错误")
 		return
 	}
 
 	// 调用model
 	list, err := new(backstage.Role).List(page, rows)
 	if err != nil {
-		r.RespErr(c, err)
+		r.RespErr(ctx, err)
 		return
 	}
 
 	// 设置返回数据
-	r.Put(c, "list", list)
+	r.Put(ctx, "list", list)
 
 	// 返回
-	r.RespOK(c)
+	r.RespOK(ctx)
 	return
 }
 
 // 用户组详情
-func (r *RoleController) Get(c *gin.Context) {
+func (r *RoleController) Get(ctx *gin.Context) {
 	// 获取参数
-	id, err := r.GetInt(c, "id")
+	id, err := r.GetInt(ctx, "id")
 	if err != nil || id < 1 {
-		r.RespErr(c, "参数id格式错误")
+		r.RespErr(ctx, "参数id格式错误")
 		return
 	}
 
@@ -66,100 +66,100 @@ func (r *RoleController) Get(c *gin.Context) {
 	roleMD := new(backstage.Role)
 	role, err := roleMD.Get(id)
 	if err != nil {
-		r.RespErr(c, err)
+		r.RespErr(ctx, err)
 		return
 	}
 
 	// 获取绑定的节点ID
 	nodeIds, err := roleMD.GetBindNodeIds(id)
 	if err != nil {
-		r.RespErr(c, err)
+		r.RespErr(ctx, err)
 		return
 	}
 
 	// 设置返回数据
-	r.Put(c, "role", role)
-	r.Put(c, "bind_node_ids", nodeIds)
+	r.Put(ctx, "role", role)
+	r.Put(ctx, "bind_node_ids", nodeIds)
 
 	// 返回
-	r.RespOK(c)
+	r.RespOK(ctx)
 	return
 }
 
 // 新增用户组
-func (r *RoleController) Add(c *gin.Context) {
+func (r *RoleController) Add(ctx *gin.Context) {
 	// 获取参数
-	name := r.GetString(c, "name")
+	name := r.GetString(ctx, "name")
 	if strLen := utf8.RuneCountInString(name); strLen == 0 || strLen > 10 {
-		r.RespErr(c, "参数name格式错误")
+		r.RespErr(ctx, "参数name格式错误")
 		return
 	}
 
-	desc := r.GetString(c, "desc", "")
-	nodeIds := r.GetString(c, "node_ids", "")
+	desc := r.GetString(ctx, "desc", "")
+	nodeIds := r.GetString(ctx, "node_ids", "")
 
 	// 调用model
 	id, err := new(backstage.Role).Add(name, desc, nodeIds)
 	if err != nil {
-		r.RespErr(c, err)
+		r.RespErr(ctx, err)
 		return
 	}
 
 	// 设置返回数据
-	r.Put(c, "id", id)
+	r.Put(ctx, "id", id)
 
 	// 返回
-	r.RespOK(c)
+	r.RespOK(ctx)
 	return
 }
 
 // 更新用户组
-func (r *RoleController) Update(c *gin.Context) {
+func (r *RoleController) Update(ctx *gin.Context) {
 	// 获取参数
-	id, err := r.GetInt(c, "id")
+	id, err := r.GetInt(ctx, "id")
 	if err != nil || id < 1 {
-		r.RespErr(c, "参数id格式错误")
+		r.RespErr(ctx, "参数id格式错误")
 		return
 	}
 
-	name := r.GetString(c, "name")
+	name := r.GetString(ctx, "name")
 	if strLen := utf8.RuneCountInString(name); strLen == 0 || strLen > 10 {
-		r.RespErr(c, "参数name格式错误")
+		r.RespErr(ctx, "参数name格式错误")
 		return
 	}
 
-	desc := r.GetString(c, "desc", "")
-	nodeIds := r.GetString(c, "node_ids", "")
+	desc := r.GetString(ctx, "desc", "")
+	nodeIds := r.GetString(ctx, "node_ids", "")
 
 	// 调用model
 	err = new(backstage.Role).Update(id, name, desc, nodeIds)
 	if err != nil {
-		r.RespErr(c, err)
+		r.RespErr(ctx, err)
 		return
 	}
 
 	// 返回
-	r.RespOK(c)
+	r.RespOK(ctx)
 	return
 }
 
 // 删除用户组
-func (r *RoleController) Delete(c *gin.Context) {
+func (r *RoleController) Delete(ctx *gin.Context) {
 	// 获取参数
-	id, err := r.GetInt(c, "id")
+	id, err := r.GetInt(ctx, "id")
 	if err != nil || id < 1 {
-		r.RespErr(c, "参数id格式错误")
+		r.RespErr(ctx, "参数id格式错误")
 		return
 	}
 
 	// 调用model
 	err = new(backstage.Role).Delete(id)
 	if err != nil {
-		r.RespErr(c, err)
+		r.RespErr(ctx, err)
 		return
 	}
 
 	// 返回
-	r.RespOK(c)
+	r.RespOK(ctx)
 	return
 }
