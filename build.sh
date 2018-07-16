075f5bf855fa5c495d40b00c779b5de8428d95a8
陈伟伟 <326087571@qq.com>
2018-07-16 18:06:35 +0800

    完善build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 # dir=/F/project
 dir=/Volumes/WorkHD/workspace/go
 
-GOOS=linux GOARCH=amd64 go build
+target=admin
 
-mkdir $dir/bin
-rm $dir/bin/*
-mv ./admin $dir/bin/
-cd $dir/bin/
-ssh root@39.108.186.82 "cd /data/ && rm admin"
-scp -r -2 $dir/bin/* root@39.108.186.82:/data/
+GOOS=linux GOARCH=amd64 go build -o ${target}
+
+mkdir ${dir}/bin
+rm ${dir}/bin/*
+mv ./${target} ${dir}/bin/
+cd ${dir}/bin/
+ssh root@39.108.186.82 "cd /data/ && rm ${target}"
+scp -r -2 ${dir}/bin/* root@39.108.186.82:/data/
 ssh root@39.108.186.82 "cd /data && ./run.sh"
