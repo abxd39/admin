5b240c05cc13be677263944ebef278bad42fb21c
陈伟伟 <326087571@qq.com>
2018-07-16 18:40:16 +0800

    完善build.sh

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ target=admin
 GOOS=linux GOARCH=amd64 go build -o ${target}
 
 mkdir ${dir}/bin
-rm ${dir}/bin/*
+rm ${dir}/bin/${target}
 mv ./${target} ${dir}/bin/
-cd ${dir}/bin/
+
 ssh root@39.108.186.82 "cd /data/ && rm ${target}"
-scp -r -2 ${dir}/bin/* root@39.108.186.82:/data/
+scp ${dir}/bin/${target} root@39.108.186.82:/data/
 ssh root@39.108.186.82 "cd /data && ./run.sh"
