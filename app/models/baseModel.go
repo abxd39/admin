42e8707a1aedf4837001661c3a5a765b0a3d1ccf
陈伟伟 <326087571@qq.com>
2018-07-16 12:04:34 +0800

    不分页列表也包装成ModelList

--- app/models/baseModel.go
+++ app/models/baseModel.go
@@ -5,16 +5,17 @@ import "math"
 type BaseModel struct {
 }
 
-// 分页列表
+// 列表
 type ModelList struct {
-	PageIndex int         `json:"page_index"`
-	PageSize  int         `json:"page_size"`
-	PageCount int         `json:"page_count"`
-	Total     int         `json:"total"`
-	Items     interface{} `json:"items"`
+	IsPage    bool        `json:"is_page"`    // 是否分页
+	PageIndex int         `json:"page_index"` // 当前页码
+	PageSize  int         `json:"page_size"`  // 每页数据条数
+	PageCount int         `json:"page_count"` // 总页数
+	Total     int         `json:"total"`      // 总数据条数
+	Items     interface{} `json:"items"`      // 数据数组
 }
 
-// 计算分页
+// 分页列表
 func (b *BaseModel) Paging(pageIndex, pageSize, total int) (offset int, modelList *ModelList) {
 	if pageIndex <= 0 {
 		pageIndex = 1
@@ -25,6 +26,7 @@ func (b *BaseModel) Paging(pageIndex, pageSize, total int) (offset int, modelLis
 	offset = (pageIndex - 1) * pageSize
 
 	modelList = &ModelList{
+		IsPage:    true,
 		PageIndex: pageIndex,
 		PageSize:  pageSize,
 		PageCount: int(math.Ceil(float64(total) / float64(pageSize))),
@@ -32,4 +34,16 @@ func (b *BaseModel) Paging(pageIndex, pageSize, total int) (offset int, modelLis
 	}
 
 	return
+}
+
+// 不分页列表，也包装成一个ModelList
+func (b *BaseModel) NoPaging(total int, list interface{}) *ModelList {
+	return &ModelList{
+		IsPage:    false,
+		PageIndex: 1,
+		PageSize:  total,
+		PageCount: 1,
+		Total:     total,
+		Items:     list,
+	}
 }
