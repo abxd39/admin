9a90a9b94c159066f9852df13b095c6c62ffa5d4
陈伟伟 <326087571@qq.com>
2018-07-17 19:49:33 +0800

    优先使用系统指定的msg

--- constant/code.go
+++ constant/code.go
@@ -13,6 +13,20 @@ const (
 	// 登录会话无效或已掉线
 	RESPONSE_CODE_SESSION_INVALID = 90600
 
-	// 登录会话被踢
-	RESPONSE_CODE_SESSION_KICK = 90601
+	// 无管理接口权限
+	RESPONSE_CODE_NO_API_PERMISSION = 90700
 )
+
+// 系统指定返回msg内容
+func GetResponseMsg(code int) string {
+	msg := ""
+
+	switch code {
+	case RESPONSE_CODE_SESSION_INVALID:
+		msg = "登录超时，请重新登录"
+	case RESPONSE_CODE_NO_API_PERMISSION:
+		msg = "对不起，你没有权限"
+	}
+
+	return msg
+}
