d3e2c6b43b6918bf2c3f52f1293adbc1203793f9
陈伟伟 <326087571@qq.com>
2018-07-20 18:42:23 +0800

    GetRemoteAddr()逻辑再优化

--- utils/request.go
+++ utils/request.go
@@ -2,21 +2,20 @@ package utils
 
 import (
 	"net"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
 // 获取用户IP
-// 如果通过nginx代理，需要设置nginx代理转发用户ip给go服务：proxy_set_header   X-Real-IP        $remote_addr;
-// 如果没代理，直接通过Request获取
 func GetRemoteAddr(ctx *gin.Context) string {
-	remoteAddr := ctx.Request.RemoteAddr
-	if ip := ctx.Request.Header.Get("X-Real-IP"); ip != "" { // nginx转发ip
+	remoteAddr, _, _ := net.SplitHostPort(ctx.Request.RemoteAddr)
+
+	// 如果有代理，上面取的addr不准确
+	if ip := ctx.Request.Header.Get("X-Forwarded-For"); ip != "" { // 最准确
+		remoteAddr = strings.Split(ip, ",")[0] // 多层代理，取第一个
+	} else if ip = ctx.Request.Header.Get("X-Real-IP"); ip != "" {
 		remoteAddr = ip
-	} else if ip = ctx.Request.Header.Get("X-Forwarded-For"); ip != "" {
-		remoteAddr = ip
-	} else {
-		remoteAddr, _, _ = net.SplitHostPort(remoteAddr)
 	}
 
 	if remoteAddr == "::1" {
