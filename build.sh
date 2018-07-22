e12615dc7e77c7e8bf3d0867820bffa93a8cef0f
陈伟伟 <326087571@qq.com>
2018-07-22 17:55:46 +0800

    优化编译脚本

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
-#! /bin/sh
+#!/bin/sh
 
 target=admin
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ${target}
-
+if [ $? -ne 0 ]; then
+    echo "编译失败"
+    exit 1
+fi
 
 os=$1
 dir=/Volumes/WorkHD/workspace/go
@@ -15,5 +18,6 @@ rm ${dir}/bin/${target}
 mv ./${target} ${dir}/bin/
 
 
+ssh root@39.108.186.82 "cd /data && ./del.sh"
 scp ${dir}/bin/${target} root@39.108.186.82:/data/
 ssh root@39.108.186.82 "cd /data && ./run.sh"
