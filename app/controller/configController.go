40abe5ffaa3eae0793ebca0e09e79ffbcdd3b153
陈伟伟 <326087571@qq.com>
2018-08-06 20:04:09 +0800

    后台配置接口

--- app/controller/configController.go
+++ app/controller/configController.go
@@ -1,7 +1,7 @@
 package controller
 
 import (
-	"admin/app/models/backstage"
+	"admin/app/models"
 	"admin/errors"
 	"encoding/json"
 	"github.com/gin-gonic/gin"
@@ -18,21 +18,25 @@ func (c *ConfigController) Router(e *gin.Engine) {
 	{
 		group.GET("/get_site", c.GetSite)
 		group.POST("/set_site", c.SetSite)
+		group.GET("/get_sms", c.GetSms)
+		group.POST("/set_sms", c.SetSms)
+		group.GET("/get_kefu", c.GetKefu)
+		group.POST("/set_kefu", c.SetKefu)
 	}
 }
 
 // 获取基础配置
 func (c *ConfigController) GetSite(ctx *gin.Context) {
 	// 调用model
-	configMD := new(backstage.Config)
-	config, err := configMD.Get(backstage.CONFIG_SITE)
+	configMD := new(models.Config)
+	config, err := configMD.Get(models.CONFIG_SITE)
 	if err != nil {
 		c.RespErr(ctx, err)
 		return
 	}
 
 	// json反解析
-	value := make(map[string]interface{})
+	value := models.SiteConfig{}
 	err = json.Unmarshal([]byte(config.Value), &value)
 	if err != nil {
 		c.RespErr(ctx, errors.NewSys(err))
@@ -117,18 +121,18 @@ func (c *ConfigController) SetSite(ctx *gin.Context) {
 	}
 
 	// 序列化成json
-	valueInterface, err := json.Marshal(map[string]interface{}{
-		"name":             name,
-		"english_name":     englishName,
-		"title":            title,
-		"english_title":    englishTitle,
-		"logo":             logo,
-		"keyword":          keyword,
-		"english_keyword":  englishKeyword,
-		"desc":             desc,
-		"english_desc":     englishDesc,
-		"beian":            beian,
-		"statistic_script": statisticScript,
+	valueBytes, err := json.Marshal(models.SiteConfig{
+		Name:            name,
+		EnglishName:     englishName,
+		Title:           title,
+		EnglishTitle:    englishTitle,
+		Logo:            logo,
+		Keyword:         keyword,
+		EnglishKeyword:  englishKeyword,
+		Desc:            desc,
+		EnglishDesc:     englishDesc,
+		Beian:           beian,
+		StatisticScript: statisticScript,
 	})
 	if err != nil {
 		c.RespErr(ctx, errors.NewSys(err))
@@ -136,13 +140,165 @@ func (c *ConfigController) SetSite(ctx *gin.Context) {
 	}
 
 	// 组织参数
-	config := &backstage.Config{
-		Name:  backstage.CONFIG_SITE,
-		Value: string(valueInterface),
+	config := &models.Config{
+		Name:  models.CONFIG_SITE,
+		Value: string(valueBytes),
 	}
 
 	// 调用model
-	err = new(backstage.Config).Set(config)
+	err = new(models.Config).Set(config)
+	if err != nil {
+		c.RespErr(ctx, err)
+		return
+	}
+
+	// 返回
+	c.RespOK(ctx)
+	return
+}
+
+// 获取短信配置
+func (c *ConfigController) GetSms(ctx *gin.Context) {
+	// 调用model
+	configMD := new(models.Config)
+	config, err := configMD.Get(models.CONFIG_SMS)
+	if err != nil {
+		c.RespErr(ctx, err)
+		return
+	}
+
+	// json反解析
+	value := models.SmsConfig{}
+	err = json.Unmarshal([]byte(config.Value), &value)
+	if err != nil {
+		c.RespErr(ctx, errors.NewSys(err))
+		return
+	}
+
+	// 设置返回数据
+	c.Put(ctx, "config", value)
+
+	// 返回
+	c.RespOK(ctx)
+	return
+}
+
+// 设置短信配置
+func (c *ConfigController) SetSms(ctx *gin.Context) {
+	// 获取参数
+	url := c.GetString(ctx, "url")
+	if strLen := utf8.RuneCountInString(url); strLen < 1 {
+		c.RespErr(ctx, "参数url格式错误")
+		return
+	}
+
+	userName := c.GetString(ctx, "user_name")
+	if strLen := utf8.RuneCountInString(userName); strLen < 1 {
+		c.RespErr(ctx, "参数user_name格式错误")
+		return
+	}
+
+	password := c.GetString(ctx, "password")
+	if strLen := utf8.RuneCountInString(password); strLen < 1 {
+		c.RespErr(ctx, "参数password格式错误")
+		return
+	}
+
+	// 序列化成json
+	valueBytes, err := json.Marshal(models.SmsConfig{
+		Url:      url,
+		UserName: userName,
+		Password: password,
+	})
+	if err != nil {
+		c.RespErr(ctx, errors.NewSys(err))
+		return
+	}
+
+	// 组织参数
+	config := &models.Config{
+		Name:  models.CONFIG_SMS,
+		Value: string(valueBytes),
+	}
+
+	// 调用model
+	err = new(models.Config).Set(config)
+	if err != nil {
+		c.RespErr(ctx, err)
+		return
+	}
+
+	// 返回
+	c.RespOK(ctx)
+	return
+}
+
+// 获取客服配置
+func (c *ConfigController) GetKefu(ctx *gin.Context) {
+	// 调用model
+	configMD := new(models.Config)
+	config, err := configMD.Get(models.CONFIG_KEFU)
+	if err != nil {
+		c.RespErr(ctx, err)
+		return
+	}
+
+	// json反解析
+	value := models.KefuConfig{}
+	err = json.Unmarshal([]byte(config.Value), &value)
+	if err != nil {
+		c.RespErr(ctx, errors.NewSys(err))
+		return
+	}
+
+	// 设置返回数据
+	c.Put(ctx, "config", value)
+
+	// 返回
+	c.RespOK(ctx)
+	return
+}
+
+// 设置短信配置
+func (c *ConfigController) SetKefu(ctx *gin.Context) {
+	// 获取参数
+	phone := c.GetString(ctx, "phone")
+	if strLen := utf8.RuneCountInString(phone); strLen < 1 {
+		c.RespErr(ctx, "参数url格式错误")
+		return
+	}
+
+	email := c.GetString(ctx, "email")
+	if strLen := utf8.RuneCountInString(email); strLen < 1 {
+		c.RespErr(ctx, "参数email格式错误")
+		return
+	}
+
+	address := c.GetString(ctx, "address")
+	if strLen := utf8.RuneCountInString(address); strLen < 1 {
+		c.RespErr(ctx, "参数address格式错误")
+		return
+	}
+
+	// 序列化成json
+	valueBytes, err := json.Marshal(models.KefuConfig{
+		Phone:   phone,
+		Email:   email,
+		Address: address,
+	})
+	if err != nil {
+		c.RespErr(ctx, errors.NewSys(err))
+		return
+	}
+
+	// 组织参数
+	config := &models.Config{
+		Name:  models.CONFIG_KEFU,
+		Value: string(valueBytes),
+	}
+
+	// 调用model
+	err = new(models.Config).Set(config)
 	if err != nil {
 		c.RespErr(ctx, err)
 		return
--- app/models/configModel.go
+++ app/models/configModel.go
@@ -1,7 +1,6 @@
-package backstage
+package models
 
 import (
-	"admin/app/models"
 	"admin/errors"
 	"admin/utils"
 	"fmt"
@@ -10,9 +9,9 @@ import (
 
 // 后台配置
 type Config struct {
-	models.BaseModel `xorm:"-"`
-	Name             string `xorm:"name pk" json:"name"`
-	Value            string `xorm:"value" json:"value"`
+	BaseModel `xorm:"-"`
+	Name      string `xorm:"name pk" json:"name"`
+	Value     string `xorm:"value" json:"value"`
 }
 
 // 表名
@@ -26,9 +25,38 @@ const (
 	CONFIG_KEFU = "kefu" // 客服配置
 )
 
+// 网站基础配置
+type SiteConfig struct {
+	Name            string `json:"name"`
+	EnglishName     string `json:"english_name"`
+	Title           string `json:"title"`
+	EnglishTitle    string `json:"english_title"`
+	Logo            string `json:"logo"`
+	Keyword         string `json:"keyword"`
+	EnglishKeyword  string `json:"english_keyword"`
+	Desc            string `json:"desc"`
+	EnglishDesc     string `json:"english_desc"`
+	Beian           string `json:"beian"`
+	StatisticScript string `json:"statistic_script"`
+}
+
+// 短信配置
+type SmsConfig struct {
+	Url      string `json:"url"`
+	UserName string `json:"user_name"`
+	Password string `json:"password"`
+}
+
+// 客服配置
+type KefuConfig struct {
+	Phone   string `json:"phone"`
+	Email   string `json:"email"`
+	Address string `json:"address"`
+}
+
 // 获取配置
 func (c *Config) Get(name string) (*Config, error) {
-	engine := utils.Engine_backstage
+	engine := utils.Engine_common
 	config := new(Config)
 	has, err := engine.Id(name).Get(config)
 	if err != nil {
@@ -48,7 +76,7 @@ func (c *Config) Set(config *Config) error {
 	config.Value = strings.Replace(config.Value, `'`, `\'`, -1)
 
 	// 开始写入，已存在就更新
-	engine := utils.Engine_backstage
+	engine := utils.Engine_common
 	_, err := engine.Exec(fmt.Sprintf("INSERT INTO %s (name, value) VALUES ('%s', '%s') ON DUPLICATE KEY UPDATE value='%[3]s'", c.TableName(), config.Name, config.Value))
 	if err != nil {
 		return errors.NewSys(err)
