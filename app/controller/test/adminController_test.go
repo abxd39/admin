a5b717485958bc60c23ac46a2eb9e25acd97171c
陈伟伟 <326087571@qq.com>
2018-08-24 16:01:15 +0800

    完善测试用例

--- app/controller/test/adminController_test.go
+++ app/controller/test/adminController_test.go
@@ -8,6 +8,32 @@ import (
 	"admin/utils/test"
 )
 
+func Test_Admin_List(t *testing.T) {
+	c := test.NewAPIClient()
+
+	// 请求参数
+	c.AddParam("page", "1")
+	c.AddParam("rows", "10")
+
+	// cookie
+	cookies := []*http.Cookie{
+		LOGIN_COOKIE,
+	}
+
+	// 发送
+	resp, err := c.Get("admin/list", cookies)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// 判断错误码
+	if resp.Code != constant.RESPONSE_CODE_OK {
+		t.Fatalf(resp.Msg)
+	}
+
+	t.Log(resp.Data)
+}
+
 func Test_Admin_Add(t *testing.T) {
 	c := test.NewAPIClient()
 
