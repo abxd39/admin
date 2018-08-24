bda88838a4f571a83703280bc3cbf24d839d0e69
陈伟伟 <326087571@qq.com>
2018-08-24 15:53:24 +0800

    ！！！添加单元测试模块！！！

---
+++ app/controller/test/adminController_test.go
@@ -0,0 +1,36 @@
+package test
+
+import (
+	"net/http"
+	"testing"
+
+	"admin/constant"
+	"admin/utils/test"
+)
+
+func Test_Admin_Add(t *testing.T) {
+	c := test.NewAPIClient()
+
+	// 请求参数
+	c.AddParam("name", TEST_ADMIN_NAME)
+	c.AddParam("nickname", TEST_ADMIN_NAME)
+	c.AddParam("pwd", "123456")
+	c.AddParam("re_pwd", "123456")
+	c.AddParam("role_ids", "1,2,3")
+
+	// cookie
+	cookies := []*http.Cookie{
+		LOGIN_COOKIE,
+	}
+
+	// 发送
+	resp, err := c.Post("admin/add", cookies)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// 判断错误码
+	if resp.Code != constant.RESPONSE_CODE_OK {
+		t.Fatalf(resp.Msg)
+	}
+}
--- main.go
+++ main.go
@@ -1,46 +1,31 @@
 package main
 
 import (
-	"os"
-	"github.com/gin-gonic/gin"
-	"github.com/gin-contrib/sessions"
-	"fmt"
+	"admin/app"
+	"admin/app/models"
 	"admin/cron"
 	"admin/utils"
-	"admin/app/models"
-	"admin/middleware"
-	"admin/session"
-	"admin/app"
+	"fmt"
+	"os"
 )
 
 func main() {
 	if os.Getenv("ADMIN_API_ENV") == "" {
 		panic("环境变量ADMIN_API_ENV未设置")
 	}
+
 	// 定时任务
 	cron.InitCron()
+	go models.DailyStart()
 
 	//启动定时器
 	//go new(models.TokenFeeDailySheet).BoottimeTimingSettlement()
 	//go new(models.WalletInoutDailySheet).BoottimeTimingSettlement()
 	//go new(models.CurencyFeeDailySheet).BoottimeTimingSettlement()
 
-	// 配置gin
-	r := gin.Default()
-	//定时任务
-	go models.DailyStart()
-
-
-	// session
-	r.Use(sessions.Sessions("mysession", session.Store))
-
-	// custom middleware
-	r.Use(middleware.JsCors())
-	r.Use(middleware.CheckLogin())
-	app.Router(r)
-
-	r.Run(fmt.Sprintf(":%d", utils.Cfg.MustInt("http", "port")))
-
+	// 启动gin
+	app.Init()
+	app.App.Run(fmt.Sprintf(":%d", utils.Cfg.MustInt("http", "port")))
 }
 
 //func main() {
